6acfd0ee94ec48788ca6cf6bccaba8cc399bce0a
Deivid Almeida <deividalmeida365@gmail.com>
2020-09-09 18:21:46 -0300

    finished routes creation

---
+++ routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const routes = express.Router();
+
+// routes // 
+routes.get('/', (req, res) => {
+     return res.redirect('/instructors');
+})
+
+routes.get('/instructors', (req, res) => {
+    return res.render('instructors/index');
+})
+
+routes.get('/members', (req, res) => {
+    return res.send('members section');
+})
+
+
+
+module.exports = routes;
