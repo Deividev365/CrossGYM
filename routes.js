0936b1016291c8b02b8f024e8e46e274b1900ef1
Deivid Almeida <deividalmeida365@gmail.com>
2020-09-11 17:53:52 -0300

    created the box-model for the intructors and its details

--- routes.js
+++ routes.js
@@ -4,15 +4,16 @@ const routes = express.Router();
 // routes // 
 routes.get('/', (req, res) => {
      return res.redirect('/instructors');
-})
+});
 
 routes.get('/instructors', (req, res) => {
     return res.render('instructors/index');
-})
+});
 
 routes.get('/members', (req, res) => {
     return res.send('members section');
-})
+});
+
 
 
 
