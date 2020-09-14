10f41ac0b2d9fafe757e5f4c10569dec4ce29e02
Deivid Almeida <deividalmeida365@gmail.com>
2020-09-14 18:14:38 -0300

    setting up the req.body to receive the user data

--- routes.js
+++ routes.js
@@ -6,21 +6,28 @@ routes.get('/', (req, res) => {
      return res.redirect('/instructors');
 });
 
+
 routes.get('/instructors', (req, res) => {
     return res.render('instructors/index');
 });
 
+
 routes.get('/instructors/create', (req, res) => {
     return res.render('instructors/create');
-})
+});
+
+// receive the datas from the user after
+// filling the gaps
+routes.post('/instructors', (req, res) => {
+
+    return res.send(req.body);
+});
+
 
 routes.get('/members', (req, res) => {
     return res.send('members section');
 });
 
-routes.post('/instructors', (req, res) => {
-    return res.send('Dados Enviados :)');
-})
 
 
 
